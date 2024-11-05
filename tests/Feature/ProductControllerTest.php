df6a28d9440a798f7d400bb023b793cc5da23700
Negociation <tiagitofranca04@gmail.com>
2024-11-05 03:11:36 -0300

    Correções de Testes unitarios com base no corpo da request

--- tests/Feature/ProductControllerTest.php
+++ tests/Feature/ProductControllerTest.php
@@ -30,36 +30,8 @@ class ProductControllerTest extends TestCase
     public function testProductInsertSuccess(): void
     {
 
-        $faker = Faker::create();
-
-        $payload = [
-            "code" => $faker->unique()->randomNumber(8),
-            "status" => "published",
-            "url" => $faker->url,
-            "creator" => $faker->userName,
-            "created_t" => $faker->unixTime,
-            "last_modified_t" => $faker->unixTime,
-            "product_name" => $faker->words(3, true),
-            "quantity" => $faker->randomFloat(2, 100, 1000) . ' g',
-            "brands" => $faker->company,
-            "categories" => $faker->words(5, true),
-            "labels" => "Contem gluten, Contém derivados de ovos, Contém ovos",
-            "cities" => "",
-            "purchase_places" => $faker->city . "," . $faker->country,
-            "stores" => $faker->company,
-            "ingredients_text" => $faker->sentence,
-            "traces" => "Frutos de casca rija,Leite,Soja",
-            "serving_size" => $faker->word . " " . $faker->randomFloat(2, 10, 50) . " g",
-            "serving_quantity" => $faker->randomFloat(2, 10, 50),
-            "nutriscore_score" => $faker->numberBetween(-10, 40),
-            "nutriscore_grade" => "d",
-            "main_category" => "en:" . $faker->word,
-            "image_url" => $faker->imageUrl,
-            "updated_at" => $faker->iso8601,
-            "created_at" => $faker->iso8601,
-            "id" => $faker->uuid,
-        ];
-
+        $payload=$this->fakeObj();
+        
         $this->withHeaders([
             'Authorization' => 'Bearer ' . env("API_KEY"),
         ])->json('put', "api/products/{$payload['code']}",$payload)
@@ -74,6 +46,8 @@ class ProductControllerTest extends TestCase
     public function testProductDeleteSuccess(): void
     {
         $payload=$this->fakeObj();
+
+        echo json_encode($payload);
 
         $this->withHeaders([
             'Authorization' => 'Bearer ' . env("API_KEY"),
@@ -93,14 +67,14 @@ class ProductControllerTest extends TestCase
         $faker = Faker::create();
 
         return [
-            "code" => $faker->unique()->randomNumber(8),
+            "code" => (string) $faker->unique()->randomNumber(8),
             "status" => "published",
             "url" => $faker->url,
             "creator" => $faker->userName,
             "created_t" => $faker->unixTime,
             "last_modified_t" => $faker->unixTime,
             "product_name" => $faker->words(3, true),
-            "quantity" => $faker->randomFloat(2, 100, 1000) . ' g',
+            "quantity" => $faker->randomFloat(2, 100, 1000),
             "brands" => $faker->company,
             "categories" => $faker->words(5, true),
             "labels" => "Contem gluten, Contém derivados de ovos, Contém ovos",
@@ -117,7 +91,6 @@ class ProductControllerTest extends TestCase
             "image_url" => $faker->imageUrl,
             "updated_at" => $faker->iso8601,
             "created_at" => $faker->iso8601,
-            "id" => $faker->uuid,
         ];
     }
 
